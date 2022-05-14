bad4a219703a226c2cf62925b219eafa962869ed
emmanuel Dinga <kefundinga@yahoo.com>
2022-05-14 14:30:48 -0600

    updates

--- main.tf
+++ main.tf
@@ -131,7 +131,7 @@ resource "aws_rds_cluster_instance" "aurora_postgresql_cluster_instance" {
 
 # Note: when consuming this module the "rds_parameter_group" variables are passed based on the version engiine
 resource "aws_db_parameter_group" "cluster_instance_parameter" {
-  name = "cluster_instance-db-parameter-group"
+  name = "${var.name}-cluster_instance-db-parameter-group"
   family      = lookup(var.aws_rds_aurora_cluster_config, "rds_parameter_group", var.rds_parameter_group)
   description = "${local.cluster_name}cluster_instance-db-parameter-group"
 
@@ -139,7 +139,7 @@ resource "aws_db_parameter_group" "cluster_instance_parameter" {
 }
 
 resource "aws_rds_cluster_parameter_group" "cluster_parameter" {
-  name = "aurora-cluster-parameter-group"
+  name = "${var.name}aurora-cluster-parameter-group"
   family      = lookup(var.aws_rds_aurora_cluster_config, "rds_parameter_group", var.rds_parameter_group)
   description = "${local.cluster_name}-aurora-cluster-parameter-group"
 
